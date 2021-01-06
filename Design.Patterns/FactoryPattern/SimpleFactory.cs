1cd372e1302c86cef5929d32d8d9de78a76b8d93
Hector Lobo <hector.lobo10000@gmail.com>
2021-01-06 17:57:44 -0600

    Factory pattern

---
+++ Design.Patterns/FactoryPattern/SimpleFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace FactoryPattern
+{
+    public class SimpleFactory
+    {
+        public Pizza CreatePizza(string type)
+        {
+            Pizza pizza = null;
+
+            if(type.Equals("cheese"))
+            {
+                pizza = new CheesePizza();
+            }
+
+            return pizza;
+        }
+    }
+}
